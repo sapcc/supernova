180953407b22890082c70e76c6c335bbd07384a2
andypf <andreas.pfau@sap.com>
2019-04-17 15:22:19 +0000

    production ready

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import logo from './logo.svg'
+import openSocket from 'socket.io-client'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [authToken, setAuthToken] = useState(null)
+
+  useEffect(() => {
+    let timer
+    
+    const renewToken = () => {
+      axios.get('/api/auth/token').then(response => {
+        const token = response.data
+        const [key,signature,timeout] = token.split('.')
+        setAuthToken(token)
+        timer = setTimeout(renewToken,(timeout-60)*1000)
+      }).catch(error => console.error(error))
+    }
+
+    timer = setTimeout(renewToken,0)
+    return () => clearTimeout(timer)
+  }, [])
+
+  useEffect(() => {
+    let socket
+    if(authToken) {
+      console.log(process.env.REACT_APP_BLACKHOLE_API_ENDPOINT, authToken)
+      socket = openSocket(process.env.REACT_APP_BLACKHOLE_API_ENDPOINT, {query: {authToken} })
+    } 
+
+    return () => socket && socket.disconnect()
+  }, [authToken])
+
+  return (
+    <div className="App">
+      <pre>{authToken}</pre>
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
-export default App;
+export default App
