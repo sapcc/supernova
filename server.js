2eaf3f4f703f912d7f18b699c1efa784dfdb4051
andypf <andreas.pfau@sap.com>
2019-04-18 11:20:40 +0000

    bugfixes in server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
   app.use(express.static(path.join(__dirname, 'build')))
   // Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
+  app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
 }
