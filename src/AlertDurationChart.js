f9447e1950c80195ad3f9c726906bdca247b5243
andypf <andreas.pfau@sap.com>
2019-06-18 10:58:03 +0000

    replace chart

--- src/AlertDurationChart.js
+++ src/AlertDurationChart.js
@@ -29,33 +29,38 @@ import moment from 'moment'
 export default class Example extends PureComponent {
   
   render() {
-    const offset = Date.now()
+    const startItem = this.props.alerts.reduce((lastDate,alert) => lastDate>moment(alert.startsAt) ? moment(alert.startsAt) : lastDate, moment())
+    const offset = startItem ? startItem.valueOf() : Date.now()
+    if(startItem) console.log('::::START::::',startItem.valueOf())
     const data = this.props.alerts.map(alert => {
       const start = moment(alert.startsAt)
       const end = moment(alert.endsAt)
 
       return {
         name: alert.annotations.summary, 
-        duration: [start.valueOf()-offset,end.valueOf()-offset],
+        duration: [((start.valueOf()-offset)), ((end.valueOf()-offset))],
 
-        label: [start.format('DD.MM.YYYY'),end.format('DD.MM.YYYY')],
+        label: [start.format('DD.MM.YYYY HH:mm'),end.format('DD.MM.YYYY HH:mm')],
         color: this.props.colors[alert.labels.severity]
       }
     })
+    
+    console.log(moment(Date.now()).diff(offset, "days"))
+
     return (
       <ComposedChart
         layout="vertical"
-        width={this.props.width}
-        height={600}
+        width={this.props.width-100}
+        height={500}
         data={data}
         margin={{
                     top: 20, right: 20, bottom: 20, left: 20,
                     }}
       >
         <CartesianGrid stroke="#f5f5f5" />
-        <XAxis type="number" />
+        <XAxis type="number" interval='preserveStartEnd' tickFormatter={(a) => moment(a+offset).format('DD.MM.YYYY HH:mm')} minTickGap={2} tickCount={moment(Date.now()).diff(offset, "days")} />
         <YAxis dataKey="name" type="category" hide={true}/>
-        <Tooltip />
+        <Tooltip labelFormatter={label => label} formatter={(value, name, props) => `${props.payload.label[0]} - ${props.payload.label[1]}`}/>
         <Legend />
         <Bar dataKey="duration" barSize={20} >
           {
