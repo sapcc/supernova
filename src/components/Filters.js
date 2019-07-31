3c2aa5b6d9fdf7d7c909d02a71363c488cdc5ecb
Esther Schmitz <esther.schmitz@sap.com>
2019-07-31 16:40:49 +0200

    Initial filters (option selection missing)

---
+++ src/components/Filters.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Col, Form, FormGroup, Label, Input } from 'reactstrap'
+import { useDispatch } from '../globalState'
+
+
+const Filters = ({filterLabels}) => {
+  // Filter out the top level labels (they will get special treatment, i.e. display, or actions)
+  const secondTierFilterLabels = Object.keys(filterLabels).filter(label => /^((?!(\bregion\b|\bseverity\b|\bstate\b)).)*$/.test(label))
+
+  const dispatch = useDispatch()
+
+  const addFilter = (event) => {
+    dispatch({type: 'ADD_FILTER', name: event.target.name, value: event.target.value})
+  }
+
+  return(
+    filterLabels ? 
+      <div className="filters">
+        <Form>
+          <div className="filter-section">
+            { ['severity', 'region', 'state'].map((topTierLabel) =>
+            <FormGroup key={ `filter-${topTierLabel}`} row>
+                <Label for={ `filter-${topTierLabel}` } sm={4}>{topTierLabel}</Label>
+                <Col  sm={8} >
+                  <Input type="select" bsSize="sm" name={topTierLabel} id={`filter-${topTierLabel}`} value={filterLabels[topTierLabel]} onChange={(e) => addFilter(e)}>
+                    <option value=""></option>
+                    <option value="eu-de-2">eu-de-2</option>
+                  </Input>
+                </Col>
+              </FormGroup>
+            )}
+          </div>
+          <div className="filter-section">
+            { secondTierFilterLabels.map((label) =>
+              <FormGroup key={ `filter-${label}`} row>
+                <Label for={ `filter-${label}` } sm={4}>{label}</Label>
+                <Col  sm={8} >
+                  <Input type="select" bsSize="sm" name={label} id={`filter-${label}`} value={filterLabels[label]} onChange={(e) => addFilter(e)}>
+                    
+                  </Input>
+                </Col>
+              </FormGroup>
+            )}
+          </div>
+        </Form>
+      </div>
+    :
+    "Loading filters"
+  )
+}
+
+export default Filters;
