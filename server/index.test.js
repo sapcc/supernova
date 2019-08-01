ebe19139fae0a8d407fc654c862badafadfdee86
andypf <andreas.pfau@sap.com>
2019-08-01 11:54:06 +0000

    add more tests

---
+++ server/index.test.js
@@ -0,0 +1,44 @@
+const request = require('supertest')
+const config = require('./configLoader')
+
+describe('Server', () => {
+  let server
+  beforeEach(() => {
+    server = require('./index')
+  })
+
+  afterEach(() => {
+    server.close()
+  })
+
+  describe('/system/rediness', () => {
+    test('should return 200', (done) => {
+      request(server).get('/system/readiness').expect(200, done)
+    })
+  })
+  
+  describe('/system/liveliness', () => {
+    test('should return 200', (done) => {
+      request(server).get('/system/liveliness').expect(200, done)
+    })
+  })
+  
+  describe('/api/config', () => {
+    test('should return 200', (done) => {
+      request(server).get('/api/config').expect(200, done)
+    })
+
+    it('returns a json object', (done) => {
+      request(server)
+        .get('/api/config')
+        .expect('Content-Type', /json/)
+        .expect(200, done)
+    })
+    
+    it('returns config', (done) => {
+      request(server)
+        .get('/api/config')
+        .expect(200, config, done)
+    })
+  })
+})
