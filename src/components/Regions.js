56644e7136bfe4cd02adf36fb61acb7e749da341
Esther Schmitz <esther.schmitz@sap.com>
2019-09-06 16:02:37 +0200

    Move raw data link. Adjust colors for active filter-pills

--- src/components/Regions.js
+++ src/components/Regions.js
@@ -18,17 +18,26 @@ export default ({items, counts, labelFilters, categories}) => {
   }, [items,activeRegions])
 
   const handleClick = (region) => {
+    console.log(activeRegions);
     if(labelFilters.settings.region.includes(region)) {
+      // dispatch({type: 'REGION_FILTER_ACTIVE', isActive: false})
       return dispatch({type: 'REMOVE_FILTER', name: 'region', value: region})
     }
+    // dispatch({type: 'REGION_FILTER_ACTIVE', isActive: true})
     dispatch({type: 'SET_VALUES_FOR_FILTER', name: 'region', values: [region]})
   }
+
+  const wrapperClassName = (region) => {
+    return labelFilters.settings.region.includes(region) ? "region-wrapper active" : "region-wrapper"
+  }
+
+
 
   return (
     <div className="regions-panel">
       {sortedRegions.map(region =>
-        <div className="region-wrapper" key={region}>
-          <div className={labelFilters.settings.region.includes(region) ? "region active" : "region"} key={region} onClick={() => handleClick(region)} >
+        <div className={wrapperClassName(region)} key={region}>
+          <div className="region" key={region} onClick={() => handleClick(region)} >
             <div className="region-name">{region}</div>
             {["critical", "warning", "info"].map((severity, index) =>
               <RegionSeverityCount key={`${severity}-${index}`} severity={severity} count={counts[region][severity]} countSilenced={counts[region][`${severity}Silenced`]} />
