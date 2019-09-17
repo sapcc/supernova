3551adab3b2d4680bfde7afdc794ca86680f4933
Esther Schmitz <esther.schmitz@sap.com>
2019-09-17 17:56:36 +0200

    Worldmap beautification part one

--- src/components/display/locations.js
+++ src/components/display/locations.js
@@ -20,64 +20,24 @@ export default {
     "regionname": "EU-DE-1",
     "lat":        48.66,
     "lon":        8.62,
-    "dx":         -30,
-    "dy":         30  
+    "dx":         5,
+    "dy":         5  
   },
   "eu-de-2": {
     "country":    "Germany",
     "regionname": "EU-DE-2",
-    "lat":        50.91,
-    "lon":        9.08,
+    "lat":        50.11,
+    "lon":        8.69,
     "dx":         -15,
-    "dy":         -40  
-  },
-  "na-us-1":{
-    "country":    "USA",
-    "regionname": "NA-US-1",
-    "lat":        39.98,
-    "lon":        -75.40,
-    "dx":         0,
-    "dy":         30  
-  },
-  "na-us-2":{
-    "country":    "USA",
-    "regionname": "NA-US-2",
-    "lat":        38.87,
-    "lon":        -104.77,
-    "dx":         -40,
-    "dy":         0 
-  },
-  "na-us-3":{
-    "country":    "USA",
-    "regionname": "NA-US-3",
-    "lat":        33.44,
-    "lon":        -112.07,
-    "dx":         -30,
-    "dy":         30  
-  },
-  "na-ca-1": {
-    "country":    "Canada",
-    "regionname": "NA-CA-1",
-    "lat":        43.66,
-    "lon":        -79.38,
-    "dx":         -30,
-    "dy":         -30  
-  },
-  "la-br-1":{
-    "country":    "Brazil",
-    "regionname": "LA-BR-1",
-    "lat":        -23.56,
-    "lon":        -46.63,
-    "dx":         -20,
-    "dy":         -20  
+    "dy":         5  
   },
   "eu-nl-1":{
     "country":    "Netherlands",
     "regionname": "EU-NL-1",
     "lat":        52.36,
     "lon":        4.895523,
-    "dx":         -42,
-    "dy":         0  
+    "dx":         0,
+    "dy":         -30  
   },
   "eu-ru-1":{
     "country":    "Russia",
@@ -87,12 +47,52 @@ export default {
     "dx":         25,
     "dy":         -20  
   },
+  "na-us-1":{
+    "country":    "USA",
+    "regionname": "NA-US-1",
+    "lat":        39.98,
+    "lon":        -75.40,
+    "dx":         -10,
+    "dy":         20  
+  },
+  "na-us-2":{
+    "country":    "USA",
+    "regionname": "NA-US-2",
+    "lat":        38.87,
+    "lon":        -104.77,
+    "dx":         -20,
+    "dy":         -20 
+  },
+  "na-us-3":{
+    "country":    "USA",
+    "regionname": "NA-US-3",
+    "lat":        33.44,
+    "lon":        -112.07,
+    "dx":         -30,
+    "dy":         10  
+  },
+  "na-ca-1": {
+    "country":    "Canada",
+    "regionname": "NA-CA-1",
+    "lat":        43.66,
+    "lon":        -79.38,
+    "dx":         -10,
+    "dy":         -40  
+  },
+  "la-br-1":{
+    "country":    "Brazil",
+    "regionname": "LA-BR-1",
+    "lat":        -23.56,
+    "lon":        -46.63,
+    "dx":         -20,
+    "dy":         10  
+  },
   "ap-ae-1":{
     "country":    "United Arab Emirates",
     "regionname": "AP-AE-1",
     "lat":        25.09,
     "lon":        55.17,
-    "dx":         0,
+    "dx":         11,
     "dy":         30  
   },
   "ap-sa-1":{
@@ -100,23 +100,23 @@ export default {
     "regionname": "AP-SA-1",
     "lat":        24.70,
     "lon":        46.68,
-    "dx":         -20,
-    "dy":         0  
+    "dx":         -11,
+    "dy":         20  
   },
   "ap-cn-1":{
     "country":    "China",
     "regionname": "AP-CN-1",
     "lat":        31.20,
     "lon":        121.47,
-    "dx":         -20,
-    "dy":         -5  
+    "dx":         -11,
+    "dy":         -15  
   },
   "ap-jp-1":{
     "country":    "Japan",
     "regionname": "AP-JP-1",
     "lat":        35.66,
     "lon":        139.77,
-    "dx":         0,
+    "dx":         25,
     "dy":         -20  
   },
   "ap-jp-2":{
@@ -124,7 +124,7 @@ export default {
     "regionname": "AP-JP-2",
     "lat":        34.68,
     "lon":        135.50,
-    "dx":         0,
+    "dx":         15,
     "dy":         20  
   },
   "ap-au-1":{
@@ -132,7 +132,7 @@ export default {
     "regionname": "AP-AU-1",
     "lat":        -33.86,
     "lon":        151.20,
-    "dx":         -30,
-    "dy":         -30  
+    "dx":         10,
+    "dy":         -20  
   }
 }
