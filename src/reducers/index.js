37a247713eefefe8ae8372a88ba440c41cc18a83
andypf <andreas.pfau@sap.com>
2019-09-23 10:37:32 +0000

    eliminate config api

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import categories from './categories'
 import labelFilters from './labelFilters'
 import alerts from './alerts'
 import silences from './silences'
+import user from './user'
 
 export default {
   categories,
   labelFilters,
   alerts,
-  silences
+  silences,
+  user
 }
