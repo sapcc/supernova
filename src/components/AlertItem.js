20093f37e8d89e598f554b3a1bdb5d389d224478
Andreas Pfau <ap@a-pfau.de>
2019-10-15 08:47:11 +0000

    virtualize alerts table to improve rendering process

---
+++ src/components/AlertItem.js
@@ -0,0 +1,137 @@
+import React, {useMemo} from 'react'
+import { Button } from 'reactstrap'
+import moment from 'moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Markup } from 'interweave'
+
+import { useDispatch } from '../lib/globalState'
+import AlertActionButtons from './AlertActionButtons'
+
+const AlertLabels = ({labelSettings,labels}) => {
+  const dispatch = useDispatch()  
+
+  const isFilterActive = (label, value) => (
+    labelSettings[label].findIndex(val => val === value) >= 0
+  )
+  const handlePillClick = (name, value) => {
+    if (isFilterActive(name, value)) {
+      dispatch({type: 'REMOVE_FILTER', name, value})          
+    } else {
+      dispatch({type: 'ADD_FILTER', name, value}) 
+    }
+  }
+
+  return <React.Fragment>
+    {Object.keys(labelSettings).map((labelKey, index) =>
+        labels[labelKey] &&
+          <span 
+            className={`filter-pill ${isFilterActive(labelKey, labels[labelKey]) ? 'active' : ''}`}
+            key={ `pill-${labelKey}` } 
+            onClick={() => handlePillClick(labelKey, labels[labelKey])}>
+            {labelKey} = {labels[labelKey]}
+            { isFilterActive(labelKey, labels[labelKey]) &&
+              <FontAwesomeIcon icon="times-circle" fixedWidth />
+            }
+          </span>
+    )}
+  </React.Fragment>
+}
+
+const AlertStatus = ({status,showAckedBy,showSilencedBy,showInhibitedBy,silencesKeyPayload}) => {
+  return (
+    <React.Fragment>
+      {status.state &&
+        <div>{status.state}</div>
+      }
+      {status.inhibitedBy && status.inhibitedBy.length ?
+          <div className="u-text-info u-text-small">
+            Inhibited by: 
+            <Button color="link" className="btn-inline-link" onClick={(e) => {e.preventDefault(); showInhibitedBy(status.inhibitedBy)}}>
+              {status.inhibitedBy}
+            </Button>
+          </div>
+        :
+        ""
+      }
+      {status.silencedBy && status.silencedBy.length ?
+          <div className="u-text-info u-text-small">
+            Silenced by: {silencesKeyPayload[status.silencedBy] 
+            ? <Button color="link" className="btn-inline-link" onClick={(e) => {e.preventDefault(); showSilencedBy(status.silencedBy)}}>
+                  {silencesKeyPayload[status.silencedBy].createdBy}
+                </Button>
+            : status.silencedBy
+            }
+          </div>
+        :
+        ""
+      }
+      {status.acknowledgements && status.acknowledgements.length>0 &&
+        status.acknowledgements.map((ack,i) => 
+          <div className="u-text-info u-text-small" key={i}>
+            Acknowledged by: 
+            <Button color="link" className="btn-inline-link" onClick={(e) => { e.preventDefault(); showAckedBy(ack)}}>
+              {ack.acknowledger.summary}
+            </Button>
+          </div>
+        )          
+      }      
+    </React.Fragment>
+  )
+}
+
+const descriptionParsed = ({text}) => {
+  if(!text) return ''
+  // urls in descriptions follow the schema: <URL|URL-NAME>
+  // Parse description and replace urls with a-tags
+  const regexUrl   = /<(http[^>|]+)\|([^>]+)>/g
+  const urlParsed  = text.replace(regexUrl, `<a href="$1">$2</a>`)
+
+  // replace text wrapped in *..* by strong tags
+  const regexBold  = /\*(.*)\*/g
+  const boldParsed = urlParsed.replace(regexBold, `<strong>$1</strong>`)
+
+  const regexCode = /`(.*)`/g
+  return boldParsed.replace(regexCode, `<code>$1</code>`)
+}
+
+const AlertItem = ({
+  alert,visible,labelSettings,silencesKeyPayload,showDetails,
+  showInhibitedBy,showSilencedBy,showAckedBy}) => {
+  
+  if(!visible) return <tr><td colSpan={6}>Loading...</td></tr>  
+
+  return (
+    <tr className={alert.labels.severity} >
+      <td className="text-nowrap">{alert.labels.region}</td>
+      <td>
+        {alert.labels.service}
+      </td>
+      <td>
+        {alert.annotations.summary}
+        <br/>
+        <small className="u-text-info">
+          <Markup content={descriptionParsed(alert.annotations.description)} tagName="span"/> - {' '}
+          <Button className="btn-inline-link" color="link" onClick={(e) => { e.preventDefault(); showDetails()}}>
+            Show raw data
+          </Button>
+        </small>
+        <br />
+        <AlertLabels labels={alert.labels} labelSettings={labelSettings}/>
+      </td>
+      <td>{moment(alert.startsAt).format('DD.MM.YYYY HH:mm:ss')}</td>
+      <td>
+        <AlertStatus 
+          status={alert.status} 
+          showAckedBy={showAckedBy}
+          showSilencedBy={showSilencedBy}
+          showInhibitedBy={showInhibitedBy}
+          silencesKeyPayload={silencesKeyPayload}/>
+      </td>
+      <td className="snug">
+        <AlertActionButtons alert={alert} />
+      </td>
+    </tr>
+  )
+}
+
+export default AlertItem
--- src/components/Alerts.js
+++ src/components/Alerts.js
@@ -1,16 +1,9 @@
-import React, {useMemo} from 'react'
-import { Button } from 'reactstrap'
-import moment from 'moment'
+import React, {useMemo,useState,useEffect,useRef} from 'react'
 import ReactJson from 'react-json-view'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Markup } from 'interweave'
-
-import { useDispatch } from '../lib/globalState'
-import AlertActionButtons from './AlertActionButtons'
+import AlertItem from './AlertItem'
 
 const Alerts = ({alerts,silences,categories,labelFilters,showModal}) => {
-
-  const dispatch = useDispatch()  
+  const tableElement = useRef(null)
   const activeLabelFilters = {}
   const labelSettings = labelFilters.settings
   const activeCategories = useMemo(() => categories.items.filter(c => c.active), [categories.items])
@@ -19,32 +12,53 @@ const Alerts = ({alerts,silences,categories,labelFilters,showModal}) => {
     , [silences.items]
   )
 
+  // These variables are necessary to calculate which alerts to render.
+  // We only render those alerts that are currently visible in the viewport of the window.
+  const tableOffset = (tableElement.current || {}).offsetTop
+  const viewportHeight = window.innerHeight
+  const tableHeight = Math.max((tableElement.current || {}).offsetHeight, viewportHeight*2)
+  const [scrollOffset,setScrollOffset] = useState(0)
+
+  // update scrollOffset on scroll event
+  useEffect(() => {
+    const supportPageOffset = window.pageXOffset !== undefined
+    const isCSS1Compat = ((document.compatMode || "") === "CSS1Compat")
+
+    window.addEventListener('scroll', () => {
+      const y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop
+      console.log('update scroll variable')
+      setScrollOffset(y)
+    })
+  },[])
+
   for(let name in labelSettings) { 
     if(labelSettings[name] && labelSettings[name].length>0) activeLabelFilters[name] = labelSettings[name] 
   }
 
-  let items = useMemo(() => {
+  // collect fingerprints of visible alerts
+  // returns a hash
+  let filteredAlertsIds = useMemo(() => {
     // don't filter at all if categories are empty
-    if(categories.active.length === 0) return alerts.items
-    
-    return alerts.items.filter(alert => {
-      return activeCategories.reduce((matchesOtherCategories,category) => {
+    if(categories.active.length === 0) return {}
+
+    return alerts.items.reduce((hash,alert) => {
+      let visible = activeCategories.reduce((matchesOtherCategories,category) => {
         return matchesOtherCategories && Object.keys(category.match_re).reduce((matchesOtherLabels,label) => {
           const regex = new RegExp(category.match_re[label])
           return matchesOtherLabels && regex.test(alert.labels[label]) 
         },true)
       },true)
-    })
-  }, [alerts,categories,activeCategories])
-    
-  if(Object.keys(activeLabelFilters).length >= 0) {
-    items = items.filter(alert => {
-      for(let name in activeLabelFilters) { 
-        if(activeLabelFilters[name].indexOf(alert.labels[name]) < 0) return false
+
+      if(visible && Object.keys(activeLabelFilters).length >= 0) {
+        for(let name in activeLabelFilters) { 
+          if(activeLabelFilters[name].indexOf(alert.labels[name]) < 0) visible = false
+        }
       }
-      return true
-    })
-  }
+
+      if(visible) hash[alert.fingerprint] = true
+      return hash
+    },{})
+  },[alerts,categories,activeCategories,activeLabelFilters])
 
   const toggleDetailsModal = (alert) => 
     showModal({
@@ -53,8 +67,7 @@ const Alerts = ({alerts,silences,categories,labelFilters,showModal}) => {
       cancelButtonText: "Close" 
     })
   
-  const toggleSilenceModal = (e,silenceId) => {
-    e.preventDefault()
+  const toggleSilenceModal = (silenceId) => {
     if(!silencesKeyPayload[silenceId]) return
     showModal({
       header: <React.Fragment>Silence</React.Fragment>,
@@ -74,8 +87,7 @@ const Alerts = ({alerts,silences,categories,labelFilters,showModal}) => {
     })
   }
   
-  const toggleAckedModal = (e,payload) => { 
-    e.preventDefault()
+  const toggleAckedModal = (payload) => { 
     showModal({
       header: <React.Fragment>Acknowledgement</React.Fragment>,
       body: <ReactJson src={payload} collapsed={2} collapseStringsAfterLength={100} />,
@@ -83,100 +95,13 @@ const Alerts = ({alerts,silences,categories,labelFilters,showModal}) => {
     })
   }
 
-  const alertStatus = (status) => {
-    return (
-      <React.Fragment>
-        {status.state &&
-          <div>{status.state}</div>
-        }
-        {status.inhibitedBy && status.inhibitedBy.length ?
-            <div className="u-text-info u-text-small">
-              Inhibited by: 
-              <Button color="link" className="btn-inline-link" onClick={() => toggleInhibitedModal(status.inhibitedBy)}>
-                {status.inhibitedBy}
-              </Button>
-            </div>
-          :
-          ""
-        }
-        {status.silencedBy && status.silencedBy.length ?
-            <div className="u-text-info u-text-small">
-              Silenced by: {silencesKeyPayload[status.silencedBy] 
-              ? <Button color="link" className="btn-inline-link" onClick={(e) => toggleSilenceModal(e,status.silencedBy)}>
-                    {silencesKeyPayload[status.silencedBy].createdBy}
-                  </Button>
-              : status.silencedBy
-              }
-            </div>
-          :
-          ""
-        }
-        {status.acknowledgements && status.acknowledgements.length>0 &&
-          status.acknowledgements.map((ack,i) => 
-            <div className="u-text-info u-text-small" key={i}>
-              Acknowledged by: 
-              <Button color="link" className="btn-inline-link" onClick={(e) => toggleAckedModal(e,ack)}>
-                {ack.acknowledger.summary}
-              </Button>
-            </div>
-          )          
-        }      
-      </React.Fragment>
-    )
-  }
-
-  const isFilterActive = (label, value) => (
-    labelSettings[label].findIndex(val => val === value) >= 0
-  )
-
-  const handlePillClick = (name, value) => {
-    if (isFilterActive(name, value)) {
-      dispatch({type: 'REMOVE_FILTER', name, value})          
-    } else {
-      addFilter(name, value)
-    }
-  }
-
-  const addFilter = (name, value) => {
-    dispatch({type: 'ADD_FILTER', name, value})    
-  }
-
-  // get white-listed filter labels, filter out the ones we show in the list anyway, then check each of the remaining ones if they exist on the given alert. If yes render a filter pill for them
-  const alertLabels = (alert) => {
-    return <React.Fragment>
-      {Object.keys(labelSettings).map((labelKey, index) =>
-          alert.labels[labelKey] &&
-            <span 
-              className={`filter-pill ${isFilterActive(labelKey, alert.labels[labelKey]) ? 'active' : ''}`}
-              key={ `pill-${labelKey}` } 
-              onClick={() => handlePillClick(labelKey, alert.labels[labelKey])}>
-              {labelKey} = {alert.labels[labelKey]}
-              { isFilterActive(labelKey, alert.labels[labelKey]) &&
-                <FontAwesomeIcon icon="times-circle" fixedWidth />
-              }
-            </span>
-      )}
-    </React.Fragment>
-  }
-
-  const descriptionParsed = (description) => {
-    if(!description) return ''
-    // urls in descriptions follow the schema: <URL|URL-NAME>
-    // Parse description and replace urls with a-tags
-    const regexUrl   = /<(http[^>|]+)\|([^>]+)>/g
-    const urlParsed  = description.replace(regexUrl, `<a href="$1">$2</a>`)
-
-    // replace text wrapped in *..* by strong tags
-    const regexBold  = /\*(.*)\*/g
-    const boldParsed = urlParsed.replace(regexBold, `<strong>$1</strong>`)
-
-    const regexCode = /`(.*)`/g
-    return boldParsed.replace(regexCode, `<code>$1</code>`)
-  }
-
+  const length = Object.keys(filteredAlertsIds).length
+  const itemHeight = Math.max(Math.ceil(tableHeight/length),50)
+  const start = Math.min(-25,Math.floor((scrollOffset-tableOffset)/itemHeight))
+  const end = Math.ceil((viewportHeight+scrollOffset)/itemHeight)
 
   return (
-    <table className="table table-main">
+    <table className="table table-main" ref={tableElement}>
       <thead>
         <tr>
           <th>
@@ -199,25 +124,19 @@ const Alerts = ({alerts,silences,categories,labelFilters,showModal}) => {
       </thead>
       <tbody>
         {/* IF NO ALERTS -> YAY */}
-        {items.map((alert,index) =>
-          <tr key={index} className={alert.labels.severity} >
-            <td className="text-nowrap">{alert.labels.region}</td>
-            <td>
-              {alert.labels.service}
-            </td>
-            <td>
-              {alert.annotations.summary}
-              <br/>
-              <small className="u-text-info"><Markup content={descriptionParsed(alert.annotations.description)} tagName="span"/> - <Button className="btn-inline-link" color="link" onClick={() => toggleDetailsModal(alert)}>Show raw data</Button></small>
-              <br />
-              {alertLabels(alert)}
-            </td>
-            <td>{moment(alert.startsAt).format('DD.MM.YYYY HH:mm:ss')}</td>
-            <td>{alertStatus(alert.status)}</td>
-            <td className="snug">
-              <AlertActionButtons alert={alert} />
-            </td>
-          </tr>
+        {alerts.items.map((alert,index) =>
+          filteredAlertsIds[alert.fingerprint] && 
+          <AlertItem 
+            key={index}
+            visible={index >= start && index <= end}
+            alert={alert}
+            labelSettings={labelSettings}
+            silencesKeyPayload={silencesKeyPayload}
+            showDetails={() => toggleDetailsModal(alert.clone())}
+            showInhibitedBy={(fingerprint) => toggleInhibitedModal(fingerprint) }
+            showSilencedBy={(silenceId) => toggleSilenceModal(silenceId)}
+            showAckedBy={(payload) => toggleAckedModal(payload)}
+          />
         )}
       </tbody> 
     </table> 
