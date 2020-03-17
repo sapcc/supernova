ef9e16a5442ab9e6f39a4542733b04db9b7e839a
Andreas Pfau <andreas.pfau@sap.com>
2020-03-17 14:16:44 +0100

    AlertManager: do not read undefined property "response" on error object

--- server/helpers/utils.js
+++ server/helpers/utils.js
@@ -1,23 +1,25 @@
 const doPeriodical = (options = {}, func) => {
-  options = Object.assign({intervalInSeconds: 300, immediate: true},options)
-  if(!options.intervalInSeconds || options.intervalInSeconds <= 0 ) options.intervalInSeconds = 300
+  options = Object.assign({ intervalInSeconds: 300, immediate: true }, options)
+  if (!options.intervalInSeconds || options.intervalInSeconds <= 0)
+    options.intervalInSeconds = 300
   let interval = options.intervalInSeconds * 1000
 
   const periodical = () => {
     let start = Date.now()
     // resolve the given function and re-queue new execution of this function.
-    // "finally" ensures that new execution takes place regardless of whether 
+    // "finally" ensures that new execution takes place regardless of whether
     // the last execution was successful or not
     Promise.resolve(func()).finally(() => {
       let timeout = start + interval - Date.now()
-      if(timeout < 0) timeout = 0
-      setTimeout(periodical,timeout)
+      if (timeout < 0) timeout = 0
+      setTimeout(periodical, timeout)
     })
   }
   setTimeout(periodical, options.immediate ? 0 : interval)
 }
 
-const capitalize = (string) => string ? string[0].toUpperCase() + string.slice(1) : ''
+const capitalize = string =>
+  string ? string[0].toUpperCase() + string.slice(1) : ""
 
 module.exports = {
   doPeriodical,
