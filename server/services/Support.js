33d59373804dd50edf6828d61eff93e9463c5c8e
Andreas Pfau <andreas.pfau@sap.com>
2020-07-15 14:06:25 +0200

    improve error handling in support service

--- server/services/Support.js
+++ server/services/Support.js
@@ -7,22 +7,26 @@ const fs = require("fs"),
     }`
   )
 
-const loadContactList = () => {
-  let json = {}
-  try {
-    json = JSON.parse(fs.readFileSync(filePath))
-  } catch (e) {
-    console.error("ERROR")
-  }
-  return json
-}
-let contactList = loadContactList()
+let contactList
 
-// This is the fs watch function.
 if (fs.existsSync(filePath)) {
-  fs.watch(filePath, (event, _) => {
-    if (event === "change") contactList = loadContactList()
-  })
+  const loadContactList = () => {
+    let json = {}
+    try {
+      json = JSON.parse(fs.readFileSync(filePath))
+    } catch (e) {
+      console.error("ERROR")
+    }
+    return json
+  }
+  contactList = loadContactList()
+
+  // This is the fs watch function.
+  if (fs.existsSync(filePath)) {
+    fs.watch(filePath, (event, _) => {
+      if (event === "change") contactList = loadContactList()
+    })
+  }
 }
 
 const getContacts = async () => contactList
