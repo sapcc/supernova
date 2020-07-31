ee0f945d861562a65fd6a6f46c2c18d40e57c094
Esther Schmitz <esther.schmitz@sap.com>
2020-07-31 10:41:25 +0200

    Try fixing the file watch by accounting for symlinks

--- server/services/Support.js
+++ server/services/Support.js
@@ -12,6 +12,30 @@ const loadData = (filePath) => {
   }
 }
 
+/** Helper for watchFile, also handling symlinks */
+function watchFile(path, callback) {
+  // Check if it's a link
+  fs.lstat(path, function(err, stats) {
+      if(err) {
+          // Handle errors
+          return callback(err);
+      } else if(stats.isSymbolicLink()) {
+          // Read symlink
+          fs.readlink(path, function(err, realPath) {
+              console.log("---- readlink");
+              // Handle errors
+              if(err) return callback(err);
+              // Watch the real file
+              fs.watch(realPath, callback);
+          });
+      } else {
+          console.log("watch");
+          // It's not a symlink, just watch it
+          fs.watch(path, callback);
+      }
+  });
+}
+
 supportFiles.forEach((file) => {
   const filePath = path.join(
     __dirname,
@@ -21,8 +45,11 @@ supportFiles.forEach((file) => {
   )
 
   if (fs.existsSync(filePath)) {
-    fs.watch(filePath, (event, _) => {
-      if (event === "change") data[file] = loadData(filePath)
+    watchFile(filePath, (event, _) => {
+      if (event === "change") {
+        console.log("+++ SUPPORT FILE CHANGE:", filePath, ". Reloading!!");
+        data[file] = loadData(filePath)
+      }
     })
 
     data[file] = loadData(filePath)
