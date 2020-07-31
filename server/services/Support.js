1930d6699fee055d2bf11b8de9ad6f1cccfeca5e
Esther Schmitz <esther.schmitz@sap.com>
2020-07-31 16:24:27 +0200

    Remove filewatch stuff. Just doesn't work

--- server/services/Support.js
+++ server/services/Support.js
@@ -6,44 +6,13 @@ const data = {}
 
 const loadData = (filePath) => {
   try {
+    console.log("Loading support file: ", filePath)
     return JSON.parse(fs.readFileSync(filePath))
   } catch (e) {
     throw e
   }
 }
 
-/** Helper for watchFile, also handling symlinks */
-function watchFile(path, callback) {
-
-  if (fs.existsSync(path)) {
-    // Check if it's a link
-    fs.lstat(path, function(err, stats) {
-        if(err) {
-            // Handle errors
-            return callback(err)
-        } else if(stats.isSymbolicLink()) {
-            // Read symlink
-            fs.readlink(path, function(err, realPath) {
-                console.log("---- readlink")
-                // Handle errors
-                if(err) return callback(err)
-                // Watch the real file if it exists
-                if (fs.existsSync(realPath)) {
-                  fs.watch(realPath, callback)
-                } else {
-                  setTimeout(() => watchFile(path, callback), 5000);
-                }
-            });
-        } else {
-            console.log("watch")
-            // It's not a symlink, just watch it
-            fs.watch(path, callback)
-        }
-    })
-  } else {
-    setTimeout(() => watchFile(path, callback), 5000)
-  }
-}
 
 supportFiles.forEach((file) => {
   const filePath = path.join(
@@ -52,13 +21,6 @@ supportFiles.forEach((file) => {
       process.env.NODE_ENV === "test" ? ".sample" : ""
     }`
   )
-
-  watchFile(filePath, (event, _) => {
-    if (event === "change") {
-      console.log("+++ SUPPORT FILE CHANGE:", filePath, ". Reloading!!");
-      data[file] = loadData(filePath)
-    }
-  })
 
   if (fs.existsSync(filePath)) {
     data[file] = loadData(filePath)
