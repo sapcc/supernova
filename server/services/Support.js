dfe37eba6747183590829d0db099891a28ccd0eb
Esther Schmitz <esther.schmitz@sap.com>
2020-07-31 15:02:57 +0200

    Try again to repair the file watch

--- server/services/Support.js
+++ server/services/Support.js
@@ -14,28 +14,33 @@ const loadData = (filePath) => {
 
 /** Helper for watchFile, also handling symlinks */
 function watchFile(path, callback) {
-  // Check if it's a link
-  fs.lstat(path, function(err, stats) {
-      if(err) {
-          // Handle errors
-          return callback(err);
-      } else if(stats.isSymbolicLink()) {
-          // Read symlink
-          fs.readlink(path, function(err, realPath) {
-              console.log("---- readlink");
-              // Handle errors
-              if(err) return callback(err);
-              // Watch the real file if it exists
-              if (fs.existsSync(realPath)) {
-                fs.watch(realPath, callback);
-              }
-          });
-      } else {
-          console.log("watch");
-          // It's not a symlink, just watch it
-          fs.watch(path, callback);
-      }
-  });
+
+  if (fs.existsSync(path)) {
+    // Check if it's a link
+    fs.lstat(path, function(err, stats) {
+        if(err) {
+            // Handle errors
+            return callback(err)
+        } else if(stats.isSymbolicLink()) {
+            // Read symlink
+            fs.readlink(path, function(err, realPath) {
+                console.log("---- readlink")
+                // Handle errors
+                if(err) return callback(err)
+                // Watch the real file if it exists
+                if (fs.existsSync(realPath)) {
+                  fs.watch(realPath, callback)
+                }
+            });
+        } else {
+            console.log("watch")
+            // It's not a symlink, just watch it
+            fs.watch(path, callback)
+        }
+    })
+  } else {
+    setTimeout(watchFile(path, callback), 5000);
+  }
 }
 
 supportFiles.forEach((file) => {
@@ -46,14 +51,14 @@ supportFiles.forEach((file) => {
     }`
   )
 
-  if (fs.existsSync(filePath)) {
-    watchFile(filePath, (event, _) => {
-      if (event === "change") {
-        console.log("+++ SUPPORT FILE CHANGE:", filePath, ". Reloading!!");
-        data[file] = loadData(filePath)
-      }
-    })
+  watchFile(filePath, (event, _) => {
+    if (event === "change") {
+      console.log("+++ SUPPORT FILE CHANGE:", filePath, ". Reloading!!");
+      data[file] = loadData(filePath)
+    }
+  })
 
+  if (fs.existsSync(filePath)) {
     data[file] = loadData(filePath)
   }
 })
