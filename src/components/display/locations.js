b66b94854651d60b0f0bb66d40bd02a20afea443
d064310 <andreas.pfau@sap.com>
2022-10-05 14:01:55 +0200

    remove region RU from the map

--- src/components/display/locations.js
+++ src/components/display/locations.js
@@ -1,145 +1,136 @@
-
 export default {
-  "staging": {
-    "country": "Germany",
-    "lat": 52.52,
-    "lon": 26.38,
-    "dx":  0,
-    "dy":  0
+  staging: {
+    country: "Germany",
+    lat: 52.52,
+    lon: 26.38,
+    dx: 0,
+    dy: 0,
   },
-  "admin": {
-    "country": "Germany",
-    "lat": 52.52,
-    "lon": -30.38,
-    "dx":  0,
-    "dy":  0
+  admin: {
+    country: "Germany",
+    lat: 52.52,
+    lon: -30.38,
+    dx: 0,
+    dy: 0,
   },
   "qa-de-1": {
-    "country": "Germany",
-    "lat": 77.55,
-    "lon": 185.60,
-    "dx": 0,
-    "dy": 0
+    country: "Germany",
+    lat: 77.55,
+    lon: 185.6,
+    dx: 0,
+    dy: 0,
   },
 
   "eu-de-1": {
-    "country":    "Germany",
-    "regionname": "EU-DE-1",
-    "lat":        48.66,
-    "lon":        8.62,
-    "dx":         4,
-    "dy":         8  
+    country: "Germany",
+    regionname: "EU-DE-1",
+    lat: 48.66,
+    lon: 8.62,
+    dx: 4,
+    dy: 8,
   },
   "eu-de-2": {
-    "country":    "Germany",
-    "regionname": "EU-DE-2",
-    "lat":        50.11,
-    "lon":        8.69,
-    "dx":         -15,
-    "dy":         5  
+    country: "Germany",
+    regionname: "EU-DE-2",
+    lat: 50.11,
+    lon: 8.69,
+    dx: -15,
+    dy: 5,
   },
-  "eu-nl-1":{
-    "country":    "Netherlands",
-    "regionname": "EU-NL-1",
-    "lat":        52.36,
-    "lon":        4.895523,
-    "dx":         0,
-    "dy":         -30  
+  "eu-nl-1": {
+    country: "Netherlands",
+    regionname: "EU-NL-1",
+    lat: 52.36,
+    lon: 4.895523,
+    dx: 0,
+    dy: -30,
   },
-  "eu-ru-1":{
-    "country":    "Russia",
-    "regionname": "EU-RU-1",
-    "lat":        55.75,
-    "lon":        37.62,
-    "dx":         25,
-    "dy":         -20  
+  "na-us-1": {
+    country: "USA",
+    regionname: "NA-US-1",
+    lat: 39.98,
+    lon: -75.4,
+    dx: -10,
+    dy: 20,
   },
-  "na-us-1":{
-    "country":    "USA",
-    "regionname": "NA-US-1",
-    "lat":        39.98,
-    "lon":        -75.40,
-    "dx":         -10,
-    "dy":         20  
+  "na-us-2": {
+    country: "USA",
+    regionname: "NA-US-2",
+    lat: 38.87,
+    lon: -104.77,
+    dx: -20,
+    dy: -20,
   },
-  "na-us-2":{
-    "country":    "USA",
-    "regionname": "NA-US-2",
-    "lat":        38.87,
-    "lon":        -104.77,
-    "dx":         -20,
-    "dy":         -20 
-  },
-  "na-us-3":{
-    "country":    "USA",
-    "regionname": "NA-US-3",
-    "lat":        33.44,
-    "lon":        -112.07,
-    "dx":         -30,
-    "dy":         10  
+  "na-us-3": {
+    country: "USA",
+    regionname: "NA-US-3",
+    lat: 33.44,
+    lon: -112.07,
+    dx: -30,
+    dy: 10,
   },
   "na-ca-1": {
-    "country":    "Canada",
-    "regionname": "NA-CA-1",
-    "lat":        43.66,
-    "lon":        -79.38,
-    "dx":         -10,
-    "dy":         -40  
+    country: "Canada",
+    regionname: "NA-CA-1",
+    lat: 43.66,
+    lon: -79.38,
+    dx: -10,
+    dy: -40,
   },
-  "la-br-1":{
-    "country":    "Brazil",
-    "regionname": "LA-BR-1",
-    "lat":        -23.56,
-    "lon":        -46.63,
-    "dx":         -20,
-    "dy":         0  
+  "la-br-1": {
+    country: "Brazil",
+    regionname: "LA-BR-1",
+    lat: -23.56,
+    lon: -46.63,
+    dx: -20,
+    dy: 0,
   },
-  "ap-ae-1":{
-    "country":    "United Arab Emirates",
-    "regionname": "AP-AE-1",
-    "lat":        25.09,
-    "lon":        55.17,
-    "dx":         10,
-    "dy":         30  
+  "ap-ae-1": {
+    country: "United Arab Emirates",
+    regionname: "AP-AE-1",
+    lat: 25.09,
+    lon: 55.17,
+    dx: 10,
+    dy: 30,
   },
-  "ap-sa-1":{
-    "country":    "Saudi Arabia",
-    "regionname": "AP-SA-1",
-    "lat":        24.70,
-    "lon":        46.68,
-    "dx":         -40,
-    "dy":         12  
+  "ap-sa-1": {
+    country: "Saudi Arabia",
+    regionname: "AP-SA-1",
+    lat: 24.7,
+    lon: 46.68,
+    dx: -40,
+    dy: 12,
   },
-  "ap-cn-1":{
-    "country":    "China",
-    "regionname": "AP-CN-1",
-    "lat":        31.20,
-    "lon":        121.47,
-    "dx":         -8,
-    "dy":         0  
+  "ap-cn-1": {
+    country: "China",
+    regionname: "AP-CN-1",
+    lat: 31.2,
+    lon: 121.47,
+    dx: -8,
+    dy: 0,
   },
-  "ap-jp-1":{
-    "country":    "Japan",
-    "regionname": "AP-JP-1",
-    "lat":        35.66,
-    "lon":        139.77,
-    "dx":         10,
-    "dy":         -20  
+  "ap-jp-1": {
+    country: "Japan",
+    regionname: "AP-JP-1",
+    lat: 35.66,
+    lon: 139.77,
+    dx: 10,
+    dy: -20,
   },
-  "ap-jp-2":{
-    "country":    "Japan",
-    "regionname": "AP-JP-2",
-    "lat":        34.68,
-    "lon":        135.50,
-    "dx":         11,
-    "dy":         11  
+  "ap-jp-2": {
+    country: "Japan",
+    regionname: "AP-JP-2",
+    lat: 34.68,
+    lon: 135.5,
+    dx: 11,
+    dy: 11,
   },
-  "ap-au-1":{
-    "country":    "Australia",
-    "regionname": "AP-AU-1",
-    "lat":        -33.86,
-    "lon":        151.20,
-    "dx":         0,
-    "dy":         -20  
-  }
+  "ap-au-1": {
+    country: "Australia",
+    regionname: "AP-AU-1",
+    lat: -33.86,
+    lon: 151.2,
+    dx: 0,
+    dy: -20,
+  },
 }
